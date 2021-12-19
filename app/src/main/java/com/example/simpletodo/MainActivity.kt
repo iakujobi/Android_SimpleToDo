0612b82dba7d195cf3d5d19c2ec5f7cf0dd103d2
Ikechukwu Akujobi <iykejobi@gmail.com>
2021-12-19 08:35:17 -0500

    variable for listOfTasks

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -6,6 +6,9 @@ import android.util.Log
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    val listOfTasks = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,5 +18,8 @@ class MainActivity : AppCompatActivity() {
 //            // Code in here is going to be executed when the user clicks on a button
 //            Log.i("Caren", "User clicked on button")
 //        }
+
+        listOfTasks.add("Do laundry")
+        listOfTasks.add("Go for a walk")
     }
 }

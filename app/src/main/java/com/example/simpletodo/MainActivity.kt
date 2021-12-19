84edc9f553f61e3e71e39e1b7cc8e0d2cc7d4512
Ikechukwu Akujobi <iykejobi@gmail.com>
2021-12-19 08:00:35 -0500

    detect user's click

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.simpletodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 1. Let's detect when the user clicks on the add button
+//        findViewById<Button>(R.id.button).setOnClickListener{
+//            // Code in here is going to be executed when the user clicks on a button
+//            Log.i("Caren", "User clicked on button")
+//        }
     }
 }

fa024afb7743e7e7087dfbf07538346d25572f07
Ikechukwu Akujobi <iykejobi@gmail.com>
2021-12-20 01:51:53 -0500

    Update TaskItemAdapter.kt

--- app/src/main/java/com/example/simpletodo/TaskItemAdapter.kt
+++ app/src/main/java/com/example/simpletodo/TaskItemAdapter.kt
@@ -1,4 +1,31 @@
 package com.example.simpletodo
 
-class TaskItemAdapter {
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * What this class does?
+ * A bridge that tells the recyclerView how to display the data we give it
+ */
+
+class TaskItemAdapter : RecyclerView.Adapter<TaskItemAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        // TODO
+    }
 }

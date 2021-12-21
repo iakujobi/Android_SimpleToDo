6461e30b48f77ec4d9d7efa1a962d687d3e0c5a0
Ikechukwu Akujobi <iykejobi@gmail.com>
2021-12-20 23:59:20 -0500

    add item and reset textField

--- app/src/main/java/com/example/simpletodo/MainActivity.kt
+++ app/src/main/java/com/example/simpletodo/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -33,5 +34,25 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = adapter
         // Set layout manager to position the items
         recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // Set up the button and input field, so that the user can enter a task and add it to the list
+
+        val inputTextField = findViewById<EditText>(R.id.addTaskField)
+
+        // Get a reference to the button and then set an onClickListener
+        findViewById<Button>(R.id.button).setOnClickListener{
+            // 1. Grab the text the user has inputted into @id/addTaskField
+            val userInputtedTask = inputTextField.text.toString()
+
+            // 2. Add the string to our list of tasks: listOfTasks
+            listOfTasks.add(userInputtedTask)
+
+            // Notify the data adapter that our data has been updated
+            adapter.notifyItemInserted(listOfTasks.size - 1)
+
+            // 3. Reset text field - without this part, the text field will not reset after adding text to list
+            inputTextField.setText("")
+
+        }
     }
 }
